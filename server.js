a6f6d89458aad05e9304d570737414cfb65fbf4d
Adrian Silva <adrianfsf10@gmail.com>
2022-03-27 08:35:08 -0300

    Refatora server frontend

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+const APP_DIST_FOLDER = 'nosso-tempero/frontend'
+const DEFAULT_PAGE = 'index.html'
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/nosso-tempero/frontend'))
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/nosso-temper/frontend/index.html')
+app.use(express.static(__dirname))
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, APP_DIST_FOLDER, DEFAULT_PAGE))
+})
+// app.use(express.static(__dirname + '/nosso-tempero/frontend'))
+// app.get('/*', (req, res) => {
+//   res.sendFile(__dirname + '/nosso-temper/frontend/index.html')
+// })
+app.use((req, res) => {
+  console.error('Resource not found')
 })
 
 app.listen(PORT, () => {
