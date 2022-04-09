fa07f61fa6f712a7a38fa25ac0c15ef517343b6e
Adrian Silva <adrianfsf10@gmail.com>
2022-04-08 22:13:11 -0300

    Resolve package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL: 'http://localhost:3000'
+  URL: process.env.URL
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  URL: 'https://nosso-tempero-backend.herokuapp.com/'
+  URL: process.env.URL
 };
